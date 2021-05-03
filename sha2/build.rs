db87fd6c016f65b3c6c9dee262340c918073b5d9
dignifiedquire <me@dignifiedquire.com>
2021-05-03 14:29:15 +0200

    setup sha2 build for m1

--- sha2/build.rs
+++ sha2/build.rs
@@ -1,14 +1,21 @@
 fn main() {
-    let target_arch = std::env::var("CARGO_CFG_TARGET_ARCH").unwrap_or_default();
+    use std::env;
+    
+    let target_arch = env::var("CARGO_CFG_TARGET_ARCH").unwrap_or_default();
+    let target_os = env::var("CARGO_CFG_TARGET_OS").unwrap_or_default();
 
     let mut build256 = cc::Build::new();
     let (sha256_path, sha512_path) = if target_arch == "x86" {
         ("src/sha256_x86.S", "src/sha512_x86.S")
     } else if target_arch == "x86_64" {
         ("src/sha256_x64.S", "src/sha512_x64.S")
+    } else if target_arch == "aarch64" && target_os == "macos" {
+        build256.flag("-march=armv8-a+crypto");
+        ("src/sha256_aarch64_macos.S", "")
     } else if target_arch == "aarch64" {
         build256.flag("-march=armv8-a+crypto");
         ("src/sha256_aarch64.S", "")
+
     } else {
         panic!("Unsupported target architecture");
     };
