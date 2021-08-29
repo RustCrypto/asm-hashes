57ef977181ace3661a55da952d2f77ffe94d7d85
Vladimir <v.petrzhikovskiy@dexpa.io>
2021-08-29 19:49:52 +0300

    Add sha256-avx2 and sha256-ni

--- sha2/build.rs
+++ sha2/build.rs
@@ -1,14 +1,29 @@
+use std::collections::HashSet;
+
 fn main() {
     use std::env;
 
     let target_arch = env::var("CARGO_CFG_TARGET_ARCH").unwrap_or_default();
     let target_vendor = env::var("CARGO_CFG_TARGET_VENDOR").unwrap_or_default();
+    // panic!("{}", env::var("CARGO_CFG_TARGET_ARCH").unwrap());
+    let features: HashSet<String> = env::var("CARGO_CFG_TARGET_FEATURE")
+        .unwrap_or_default()
+        .split(',')
+        .map(|x| x.to_string())
+        .collect();
 
     let mut build256 = cc::Build::new();
     let (sha256_path, sha512_path) = if target_arch == "x86" {
         ("src/sha256_x86.S", "src/sha512_x86.S")
     } else if target_arch == "x86_64" {
-        ("src/sha256_x64.S", "src/sha512_x64.S")
+        // using aes-ni, cause it's fastest
+        if features.contains("aes") {
+            ("src/sha256_x64_ni.S", "src/sha512_x64.S")
+        } else if features.contains("avx2") {
+            ("src/sha256_x64_avx2.S", "src/sha512_x64.S")
+        } else {
+            ("src/sha256_x64.S", "src/sha512_x64.S")
+        }
     } else if target_arch == "aarch64" && target_vendor == "apple" {
         build256.flag("-march=armv8-a+crypto");
         ("src/sha256_aarch64_apple.S", "")
--- sha2/src/lib.rs
+++ sha2/src/lib.rs
@@ -14,10 +14,35 @@
 compile_error!("crate can only be used on x86, x86-64 and aarch64 architectures");
 
 #[link(name = "sha256", kind = "static")]
+#[allow(dead_code)]
 extern "C" {
     fn sha256_compress(state: &mut [u32; 8], block: &[u8; 64]);
+    #[cfg(target_feature = "avx2")]
+    fn sha256_transform_rorx(state: &mut [u32; 8], block: *const [u8; 64], num_blocks: usize);
+    #[cfg(target_feature = "aes")]
+    fn sha256_ni_transform(digest: &mut [u32; 8], data: *const [u8; 64], nblk: u64);
 }
 
+#[cfg(not(target_feature = "aes"))]
+#[cfg(target_feature = "avx2")]
+#[inline]
+pub fn compress256(state: &mut [u32; 8], blocks: &[[u8; 64]]) {
+    if !blocks.is_empty() {
+        unsafe { sha256_transform_rorx(state, blocks.as_ptr(), blocks.len()) }
+    }
+}
+
+#[cfg(target_feature = "aes")]
+#[cfg(not(target_feature = "avx2"))]
+#[inline]
+pub fn compress256(state: &mut [u32; 8], blocks: &[[u8; 64]]) {
+    if !blocks.is_empty() {
+        unsafe { sha256_ni_transform(state, blocks.as_ptr(), blocks.len() as u64) }
+    }
+}
+
+#[cfg(not(target_feature = "aes"))]
+#[cfg(not(target_feature = "avx2"))]
 /// Safe wrapper around assembly implementation of SHA256 compression function
 #[inline]
 pub fn compress256(state: &mut [u32; 8], blocks: &[[u8; 64]]) {
