efec464c638a4bdf94b8c1d5bd96a50da90a9fc3
Vladimir <v.petrzhikovskiy@dexpa.io>
2021-09-05 13:55:29 +0300

    Refactor build script

--- sha2/build.rs
+++ sha2/build.rs
@@ -5,23 +5,23 @@ fn main() {
     let target_vendor = env::var("CARGO_CFG_TARGET_VENDOR").unwrap_or_default();
     let target_os = env::var("CARGO_CFG_TARGET_OS").unwrap_or_default();
     let mut build256 = cc::Build::new();
-    let (sha256_path, sha512_path) = if target_arch == "x86" {
-        (["src/sha256_x86.S"].iter(), ["src/sha512_x86.S"].iter())
+    let (sha256_path, sha512_path): (&[&str], &[&str]) = if target_arch == "x86" {
+        (&["src/sha256_x86.S"], &["src/sha512_x86.S"])
     } else if target_arch == "x86_64" {
         if target_os == "linux" {
             (
-                ["src/sha256_x64_avx2.S", "src/sha256_x64.S"].iter(),
-                ["src/sha512_x64_avx2.S", "src/sha512_x64.S"].iter(),
+                &["src/sha256_x64_avx2.S", "src/sha256_x64.S"],
+                &["src/sha512_x64_avx2.S", "src/sha512_x64.S"],
             )
         } else {
-            (["src/sha256_x64.S"].iter(), ["src/sha512_x64.S"].iter())
+            (&["src/sha256_x64.S"], &["src/sha512_x64.S"])
         }
     } else if target_arch == "aarch64" && target_vendor == "apple" {
         build256.flag("-march=armv8-a+crypto");
-        (["src/sha256_aarch64_apple.S"].iter(), [""].iter())
+        (&["src/sha256_aarch64_apple.S"], &[""])
     } else if target_arch == "aarch64" {
         build256.flag("-march=armv8-a+crypto");
-        (["src/sha256_aarch64.S"].iter(), [""].iter())
+        (&["src/sha256_aarch64.S"], &[""])
     } else {
         panic!("Unsupported target architecture");
     };
